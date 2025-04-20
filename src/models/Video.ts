53097bf972c38390b0af55e4906d0160fac86ab8
Utkarsh Lanjewar <lanjewarutkarsh@gmail.com>
2025-04-20 17:56:54 +0530

    Added publicUrl in schema

--- src/models/Video.ts
+++ src/models/Video.ts
@@ -17,6 +17,7 @@ const schema = new mongoose.Schema({
     status: String,
     videoUrl: String,
     audioUrl: String,
+    publicUrl : String,
     fileName : String,
     quiz: [quizSchema]
   }, 
